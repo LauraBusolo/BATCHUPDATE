df8c90689d95156744a7a16ecec2e048a356ad26
Laura <laura@quarticsolutions.com>
2018-01-17 14:57:08 -0800

    It works!

--- BatchUpdate.py
+++ BatchUpdate.py
@@ -1,18 +1,23 @@
-#-------------------------------------------------------------------------------
-# Name:        module2
+# Name:        Batch Processing
 # Purpose:
-#
-# Author:      msgis-student
-#
-# Created:     17/01/2018
-# Copyright:   (c) msgis-student 2018
-# Licence:     <your licence>
+"""Create field to shp or fc, then Copy/Update feature class/shp to a different source/fgdb
+"""
+
+# Author:      Laura
 #-------------------------------------------------------------------------------
 #Importing modules
 import arcpy
 
+arcpy.env.workspace = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb"
+lyrUpdate_table = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb\Layer_Update"
+arcpy.overwriteOutput = True
+field_names = ['BatchID', 'SourcePath', 'SourceName', 'TargetPath', 'TargetName', 'Method']
+batch_Num = raw_input ("Please enter Batch ID: ")
+
 #Define the copy features function
 def copy_features(input_table, out_feature_class):
+    print (input_table)
+    print (out_feature_class)
     try:
         if arcpy.Exists(out_feature_class):
             arcpy.Delete_management(out_feature_class)
@@ -24,49 +29,30 @@ def copy_features(input_table, out_feature_class):
         print err.message
         print "Error occurred while copying feature(s)"
 
-#Setting up workspace
-arcpy.env.workspace = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb"
-#arcpy.env.workspace = raw_input("Please enter the path of your workspace")
-input_table = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb\Layer_Update"
-#input_table = raw_input("Please enter the path of your input table")
-arcpy.overwriteOutput = True
-field_names = ['BatchID', 'SourcePath', 'SourceName', 'TargetPath', 'TargetName', 'Method']
-batch_Num = 'BatchID'
-t_fieldList= arcpy.ListFields (input_table)
-
-###Get field names from in_table
-##for field in t_fieldList:
-##    print field.name
-
-##BatchID_Entry = raw_input('Please enter Batch ID: ')
-
-###Creating the search cursor to read throw the rows in the table
-##rows = arcpy.da.SearchCursor(in_table)
-## Call SearchCursor.next() to read the first row
-##row = rows.next()
-##
-##while row:
-##    print row.BatchID
-##    row = rows.next()
 
 #(Specify Query) Check table and access rows with BatchID as 1:
-cursor = arcpy.da.SearchCursor(input_table, field_names, "\"Batch_ID\" = 1")
+cursor = arcpy.da.SearchCursor(lyrUpdate_table, field_names, "\"BatchID\" = "+ str(batch_Num))
 
-
-with arcpy.da.SearchCursor(input_table, field_names) as cursor:
+with arcpy.da.SearchCursor(lyrUpdate_table, field_names) as cursor:
 
     for row in cursor:
-        print "Access granted"
+        #a = row.getValue("Batch_ID")
+        #Access the data using indices, e.g BatchID's index is 0.
+##        a = row [0]
+        print row
+        batch_id, source_path, source_name, target_path, target_name, method= row
+        print ("C is "+ target_path)
 
+        #Access data using tuple logic
 
+        copy_features(source_path + '\\' + source_name, target_name)
 
-###Get Field names from table
-##TableFields = arcpy.ListFields (in_table)
-##for field in TableFields:
-##    print field.name
 
 #If Batch_ID == 1, execute copy features function
 
 # Call the Copy features function
 
-#copy_features()
+"""
+- Add print statements, to show the steps, methods...
+- try copying ny adding records to the table
+"""
