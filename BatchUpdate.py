5bd05d746d49cd53c3fdece68c5db0643bc4a462
Laura <laura@quarticsolutions.com>
2018-01-18 15:27:02 -0800

    Update Works!

--- BatchUpdate.py
+++ BatchUpdate.py
@@ -8,44 +8,51 @@
 #Importing modules
 import arcpy
 
+print ("Setting up workspace...\n")
 arcpy.env.workspace = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb"
-lyrUpdate_table = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb\Layer_Update"
 arcpy.overwriteOutput = True
+#Setting up global variables
+lyrUpdate_table = r"C:\Users\msgis-student\Documents\Quartic solutions\Batch Processing\Batch Processing.gdb\Layer_Update"
 field_names = ['BatchID', 'SourcePath', 'SourceName', 'TargetPath', 'TargetName', 'Method']
 batch_Num = raw_input ("Please enter Batch ID: ")
 
 #Define the copy features function
 def copy_features(input_table, out_feature_class):
-    print (input_table)
-    print (out_feature_class)
+##    print (input_table)
+##    print (out_feature_class)
+    print ("Checking if feature(s) exist in the database...\n")
     try:
         if arcpy.Exists(out_feature_class):
             arcpy.Delete_management(out_feature_class)
 
 #Methods for updating, e.g. Copy Features, Feature class conversion
+        print ("Copying features...\n")
         arcpy.CopyFeatures_management (input_table, out_feature_class)
-        print "Copy Features Complete!"
+        print ("Copy Features Complete!\n")
     except Exception as err:
         print err.message
-        print "Error occurred while copying feature(s)"
+        print ("Error occurred while copying feature(s)\n")
 
 
-#(Specify Query) Check table and access rows with BatchID as 1:
+#(Specify Query) Check table and access rows with the input Batch ID as 1:
 cursor = arcpy.da.SearchCursor(lyrUpdate_table, field_names, "\"BatchID\" = "+ str(batch_Num))
 
+print ("Checking the table rows using the Search cursor...\n")
 with arcpy.da.SearchCursor(lyrUpdate_table, field_names) as cursor:
 
     for row in cursor:
         #a = row.getValue("Batch_ID")
         #Access the data using indices, e.g BatchID's index is 0.
 ##        a = row [0]
-        print row
+##        print row
+        #Setting up the row/field name variables...
         batch_id, source_path, source_name, target_path, target_name, method= row
-        print ("C is "+ target_path)
+        print ("C is the "+ target_path)
 
         #Access data using tuple logic
-
+        print ("Executing the copy features function...\n")
         copy_features(source_path + '\\' + source_name, target_name)
+        print ("Successfully copied features_o_ _o_ _o_")
 
 
 #If Batch_ID == 1, execute copy features function
@@ -54,5 +61,5 @@ with arcpy.da.SearchCursor(lyrUpdate_table, field_names) as cursor:
 
 """
 - Add print statements, to show the steps, methods...
-- try copying ny adding records to the table
+- try adding more records to the table.
 """
