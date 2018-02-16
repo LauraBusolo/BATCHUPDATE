6b2d9457314ac48cc96fe18f5f82a53434fe34a3
Laura <laura@quarticsolutions.com>
2018-02-16 09:37:58 -0800

    Batch Update

--- BatchUpdate.py
+++ BatchUpdate.py
@@ -20,8 +20,7 @@ FIELD_NAMES = ['BatchID', 'SourcePath', 'SourceName', 'TargetPath', 'TargetName'
 #opening a dataset in target GDB
 out_dataset_path = r".\Batch Processing.gdb"
 out_nameFD = "newFD"
-sr = r"C:\Users\Laura\Documents\Quartic solutions\Batch Processing\Test Data\test1.gdb\SanTest\SDiegoCity_1"
-
+CoordSys = r"C:\Users\Laura\Documents\Quartic solutions\Batch Processing\Test Data\test1.gdb\SanTest\SDiegoCity_1"
 
 #Define the copy features function
 def copy_features(input_table, out_feature_class):
@@ -31,8 +30,9 @@ def copy_features(input_table, out_feature_class):
             arcpy.Delete_management(out_feature_class)
 
         # Execute CreateFeaturedataset
+        ##Not necessary
         if not arcpy.Exists(out_nameFD):
-            arcpy.CreateFeatureDataset_management(out_dataset_path, out_nameFD, sr)
+            arcpy.CreateFeatureDataset_management(out_dataset_path, out_nameFD, CoordSys)
 
         # Methods for updating, e.g. Copy Features, Feature class conversion
         arcpy.CopyFeatures_management (input_table, out_feature_class)
@@ -45,28 +45,32 @@ def copy_features(input_table, out_feature_class):
 
 def countRows(input_table, out_feature_class):
     print ("Executing the countRows ...\n")
-    print ("input_table, out_feature_class : " + input_table + ", " + out_feature_class)
+    print ("Source data: input_table, Copied/target data: out_feature_class : " + input_table + ", " + out_feature_class)
     rows = arcpy.SearchCursor(input_table)
     row = rows.next()
+    #input_table_result is the source data
     input_table_result = 0
     while row:
         input_table_result += 1
         row = rows.next()
-    print ("input_table_result count is " + str(input_table_result) + "\n")
+    print ("Source data, i.e. input_table_result count is " + str(input_table_result) + "\n")
     rows2 = arcpy.SearchCursor(out_feature_class)
     row2 = rows2.next()
+    #out_feature_class_result is the target
     out_feature_class_result = 0
     while row2:
         out_feature_class_result += 1
         row2 = rows2.next()
-    print ("out_feature_class_result count is " + str(out_feature_class_result) + "\n")
+    print ("Copied/target data, i.e. out_feature_class_result count is " + str(out_feature_class_result) + "\n")
 
     Result_Difference = int(input_table_result) - int(out_feature_class_result)
     Result_Ratio = (Result_Difference / out_feature_class_result) * 100
     if (Result_Ratio < 20.00):
+
         print ("Not much changes")
     else:
-        print ("Over 20% changes")
+
+        print ("The data has over 20% changes")
 
 #(Specify Query) Check table and access rows with the input Batch ID as 1,2...n:
 def iterate_update_table(table_Lyr, flds, btch_num):
@@ -90,6 +94,11 @@ def iterate_update_table(table_Lyr, flds, btch_num):
 
             #Access data using tuple logic
             if batch_id == int(btch_num):
+
+                print ("Computing the difference...\n")
+                if arcpy.Exists(target_path + '\\' + target_name):
+                    countRows(source_path + '\\' + source_name, target_path + '\\' + target_name)
+
                 print ("Executing the copy features function...\n")
                 copy_features(source_path + '\\' + source_name, target_name)
 
